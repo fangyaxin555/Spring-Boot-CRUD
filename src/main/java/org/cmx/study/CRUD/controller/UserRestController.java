ee2dcc60738018e59eba01cb0d3a6ec95b0e076e
fanxingxiao <1318188724@qq.com>
2018-09-03 23:22:05 +0800

    springBoot实现Reseful风格的增删改查

--- src/main/java/org/cmx/study/CRUD/controller/UserRestController.java
+++ src/main/java/org/cmx/study/CRUD/controller/UserRestController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
+=======
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,42 +24,74 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 @RestController
+<<<<<<< HEAD
 @RequestMapping(value = "/user")
+=======
+//@RequestMapping(value = "/user")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 public class UserRestController {
 	@Autowired
 	private UserService userService;
 
+<<<<<<< HEAD
 	@PostMapping("/address")
+=======
+	@PostMapping("/user")
+//	@PostMapping("/address")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 	public boolean addUser(User user) {
 		System.out.println("开始新增...");
 		return userService.addUser(user);
 	}
 
+<<<<<<< HEAD
 	@PutMapping("/update")
+=======
+	@PutMapping("/user")
+//	@PutMapping("/update")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 	public boolean updateUser(User user) {
 		System.out.println("开始更新...");
 		return userService.updateUser(user);
 	}
 
+<<<<<<< HEAD
 	@DeleteMapping("/delete/{id}")
+=======
+	@DeleteMapping("/user/{id}")
+//	@DeleteMapping("/delete/{id}")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 	public boolean delete(@PathVariable("id") Integer id) {
 		System.out.println("开始删除...");
 		return userService.deleteUser(id);
 	}
 
+<<<<<<< HEAD
 	@GetMapping("/findByName/{name}")
+=======
+	@GetMapping("/user/{name}")
+//	@GetMapping("/findByName/{name}")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 	public User findByUserName(@PathVariable("name") String name) {
 		System.out.println("开始查询...");
 		return userService.findUserByName(name);
 	}
 
+<<<<<<< HEAD
 	@GetMapping("/findAll")
+=======
+	@GetMapping("/user")
+//	@GetMapping("/findAll")
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 	public List<User> findByUserAge() {
 		System.out.println("开始查询所有数据...");
 		return userService.findAll();
 	}
+<<<<<<< HEAD
 	@RequestMapping({"","/","/index"})
 	public String toIndex(){
 		return "index";
 	}
+=======
+>>>>>>> springboot集成mybatis实现resetful风格的增删改查
 }
